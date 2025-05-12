701e095af29f0689ae6c043805edcb4c3a915813
eri-bga <godseeshears@gmail.com>
2025-05-12 14:40:17 +0200

    Initial commit

---
+++ src/StringUtils.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace PhpEightFour\StringUtils;
+
+class StringUtils
+{
+    public static function toCamelCase(string $input): string
+    {
+        return lcfirst(self::toPascalCase($input));
+    }
+
+    public static function toPascalCase(string $input): string
+    {
+        return str_replace(' ', '', ucwords(implode(' ', preg_split('/[^a-zA-Z0-9]+/', $input))));
+    }
+
+    public static function toSnakeCase(string $input): string
+    {
+        return strtolower(preg_replace('/(?<!^)[A-Z]/', '_$0', $input));
+    }
+
+    public static function toKebabCase(string $input): string
+    {
+        return strtolower(preg_replace('/(?<!^)[A-Z]/', '-$0', $input));
+    }
+
+    public static function isPalindrome(string $input): bool
+    {
+        $cleaned = strtolower(preg_replace('/[^a-zA-Z0-9]/', '', $input));
+        return $cleaned === strrev($cleaned);
+    }
+}
